8bad1f9046925b3849330e1731c63eb92c2d9087
Birhanu <birhanumatebe12@gmail.com>
2025-11-25 16:25:19 +0300

    refactor: parse date

--- setup.py
+++ setup.py
@@ -5,4 +5,9 @@ setup(
     version='0.1.0',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
+    include_package_data=True,
+    package_data={
+        # '' means relative to each package
+        'nova_financial_insights': ['../../notebooks/*.ipynb'],
+    },
 )
